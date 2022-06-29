b05ccf2bc8f96b286da33129cf723e6772b7c999
unknown <gm.c.burzo@gmail.com>
2022-06-29 19:48:22 -0400

    Alpha1

---
+++ vue-project/src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <!--You can do @click instead of v-on:click-->
+    <button 
+        @click = "onClick()"
+        :style = "{background: color}" 
+        class = "btn">{{text}}</button>
+</template>
+
+<script>
+    export default{
+        name: 'Button',
+        props: {
+            text: String,
+            color: String
+        },
+        methods:{
+            onClick(){
+                console.log('click');
+            }
+        }
+    }
+</script>
